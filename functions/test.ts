c1d2d4e828a0ae45f6a72234928c0e7d767a13e5
Guilherme Rodrigues <gadr90@gmail.com>
2017-09-14 20:01:48 -0300

    Add example

--- functions/test.ts
+++ functions/test.ts
@@ -1,3 +1,7 @@
+import fetch from 'node-fetch'
+
+const url ='https://api.vtexcrm.com.br/semparar/dataentities/LM/search?_fields=codigo,nomedamarca,destaques&_sort=nomedamarca'
+
 export default async function (args, context) {
-  return 'hello test!'
+  return fetch(url).then((res) => res.json())
 }
