faa8e708197055ef7136074c968b48ecfb97099f
ygorops <ygorsantos92@gmail.com>
2017-09-15 15:02:43 -0300

    change sample

--- functions/get-rest-api.ts
+++ functions/get-rest-api.ts
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 
-const url ='https://api.vtexcrm.com.br/semparar/dataentities/LM/search?_fields=codigo,nomedamarca,destaques&_sort=nomedamarca'
+const url ='https://raw.githubusercontent.com/vtex-apps/functions-getting-started/master/manifest.json'
 
 export default async function (args, context) {
   return fetch(url).then((res) => res.json())
